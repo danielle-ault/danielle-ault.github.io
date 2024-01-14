8c9e312c50409ee74ed8ad8dc585d3d5b6a4d253
Danielle Ault <danielle.a.ault@gmail.com>
2024-01-13 20:09:39 -0600

    Testing pushing to web page repo

--- built/app.js
+++ built/app.js
@@ -1,5 +1,6 @@
 main();
 function main() {
     var mainText = document.getElementById("mainText");
-    mainText.textContent += "<br><br>this was added by typescript.";
+    mainText.innerHTML += "<br><br><b>this</b> was added by typescript.<br><br>";
+    mainText.innerHTML += "this text was added by pushing from VS code.";
 }
