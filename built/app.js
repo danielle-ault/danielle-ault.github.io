9f3e44d5be8a45724db84449a918a83477c42873
Danielle Ault <danielle.a.ault@gmail.com>
2024-01-13 19:10:48 -0600

    Initial test page.

---
+++ built/app.js
@@ -0,0 +1,5 @@
+main();
+function main() {
+    var mainText = document.getElementById("mainText");
+    mainText.textContent += "<br><br>this was added by typescript.";
+}
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+
+main();
+
+function main(): void
+{
+	var mainText = document.getElementById("mainText");
+	mainText.textContent += "<br><br>this was added by typescript.";
+}
